a796c3d1f458d83687bf0fb51cf0dc98c6476e88
Romain <startcorpo@gmail.com>
2018-02-06 13:32:12 +0100

    bjg

--- Widget/Widget-Commercial/src/App.js
+++ Widget/Widget-Commercial/src/App.js
@@ -10,11 +10,12 @@ class App extends Component {
   }
 
   componentDidMount() {
-    var width = 550,
-    height = 430,
-    backwidth = width+50,
-    backheight = height+70,
-    data = [1,-4,7,-6,2,8,-5,4];
+    var width = 600,
+    height = 400,
+    green = "#2cd364",
+    red = "#ea445f",
+    backheight = height+120,
+    data = [-49,24,92,88,31,-26,-9,0,47];
 
 var y = d3.scaleLinear()
     .range([height, height/2]);
@@ -27,51 +28,84 @@ var chart = d3.select(".chart")
       .append("img")
       .attr("src",background)
       .attr("height",backheight)
-      .attr("width",backwidth);
+      .attr("width",width);
 
   // y.domain([d3.min(data, function(d) { return d; }), d3.max(data, function(d) { return d; })]);
-  y.domain([-10, 10]);
+  y.domain([0, 100]);
 
 
-  var barWidth = (width-20) / data.length ;
+  var barWidth = (width-40) / data.length ;
 
   var bar = chart.selectAll("g")
       .data(data)
       .enter()
       .append("g")
       .attr("height",height)
-      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ", 0)"; });
+      .attr("transform", function(d, i) { return "translate(" + (i * barWidth + 30)+", 0)"; });
 
   bar.append("rect")
     .attr("class","empty")
     .attr("y",0)
-    .attr("rx",5)
-    .attr("ry",5)
-    .attr("fill","darkgray")
+    .attr("rx",4)
+    .attr("ry",4)
+    .attr("fill","#ccc9c7")
     .attr("height",height)
     .attr("width",barWidth - 20);
 
   bar.append("rect")
-      .attr("class","plot")
+      .attr("fill", (d) => d>=0 ? green : red)
       .attr("y", function(d) { return d>0 ? y(Math.abs(d)) - height/2 : height/2;})
-      .attr("height", function(d) { return height - y(Math.abs(d)); })
+      .attr("height", function(d) { return d==0 ? 0 : height - y(Math.abs(d)); })
       .attr("width", barWidth - 20)
-      .attr("rx",5)
-      .attr("ry",5);
+      .attr("rx",4)
+      .attr("ry",4);
 
-  bar.append("text")
-      .attr("x", barWidth / 2)
-      .attr("y", function(d) { return d>0 ? y(d) + 10 - height/2 : height - y(-d) + height/2 - 25 ; })
-      .attr("dy", ".75em")
-      .text(function(d) { return d; });
+    chart.append("line")
+      .attr("stroke","rgb(255, 120, 48)")
+      .attr("x1",0)
+      .attr("y1",height/2)
+      .attr("x2",width)
+      .attr("y2",height/2)
+      .attr("stroke-width","5");
 
-      chart.append("line")
+      let i=-100;
+      while (i <= 100) {
+        chart.append("line")
         .attr("stroke","rgb(255, 120, 48)")
         .attr("x1",0)
-        .attr("y1",height/2)
-        .attr("x2",width-40)
-        .attr("y2",height/2)
-        .attr("stroke-width","5");
+        .attr("stroke-dasharray","5,5")
+        .attr("y1",i==0 ? height/2 : i>0 ? y(i) - height/2: height - y(-i) + height/2)
+        .attr("x2",width)
+        .attr("y2",i==0 ? height/2 : i>0 ? y(i) - height/2 :height - y(-i) + height/2)
+        .attr("stroke-width","1");
+
+        chart.append("text")
+          .attr("x",20)
+          .attr("y",i==0 ? height/2 : i>0 ? y(i) - height/2 : height - y(-i) + height/2)
+          .attr("dy",".25em")
+          .attr("fill","rgb(255, 120, 48)")
+          .attr("font-weight","bold")
+          .text(Math.abs(i)+"A");
+
+        i=i+20;
+
+      }
+
+      bar.append("text")
+          .attr("class","label")
+          .attr("font-weight","bold")
+          .attr("fill",(d) => d == 0 ? "white" : d>0 ? green : red)
+          .attr("x", (barWidth-20) / 2)
+          .attr("y", function(d) { return d==0 ? height/2 - 20 : d>0 ? y(d) - 15 - height/2 : height - y(-d) + height/2 + 5 ; })
+          .attr("dy", ".75em")
+          .text(function(d) { return Math.abs(d)+" A"; });
+
+      bar.append("text")
+        .attr("x", (barWidth-20) / 2)
+        .attr("y",height+5)
+        .attr("dy",".75em")
+        .attr("fill","#ccc9c7")
+        .text((d) => "C°"+(data.indexOf(d)+1));
     }
 
   render() {
@@ -81,8 +115,13 @@ var chart = d3.select(".chart")
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to betterie</h1>
         </header>
+        <div className="panel panel-default">
+        <div className="panel-body">
+        <div className="backgroundHeader"/>
         <div className="background">
           <svg className="chart" style={{ position : "absolute"}}/>
+        </div>
+        </div>
         </div>
       </div>
     );
