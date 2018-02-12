d5f954d64c746974f4cd6498301300da75b5d017
Romain Dembele <rom.dembele@orange.fr>
2018-02-12 10:13:36 +0100

    lol
    l

--- Widget/Widget-Batterie/index.js
+++ Widget/Widget-Batterie/index.js
@@ -1,80 +1,95 @@
-createBackground = function(width, height) {
-  var w = width / 304,
-  h = height / 464;
-  var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+self.onInit = function () {
+    createView(self.ctx.width, self.ctx.height);
+};
 
+self.onDataUpdated = function () {
+};
+
+self.onResize = function () {
+    var myNode = document.getElementById("BatteryStateView");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild);
+    }
+    createView(self.ctx.width, self.ctx.height);
+};
+
+self.onDestroy = function () {
+};
+
+createBackground = function(width, height) {
+  var w = 304, h = 464 ;
+  var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
   // <rect x="22" y="68" width="260" height="57" rx="13.11" ry="13.11" fill="#cccccc" stroke="none" pointer-events="none"/>
   var rect1 = document.createElementNS("http://www.w3.org/2000/svg","rect");
-  rect1.setAttributeNS("null","x",22*w);
-  rect1.setAttributeNS("null","y",68*h);
-  rect1.setAttributeNS("null","width",260*w);
-  rect1.setAttributeNS("null","height",57*h);
-  rect1.setAttributeNS("null","rx",13.11*w);
-  rect1.setAttributeNS("null","ry",13.11*h);
+  rect1.setAttributeNS("null","x",Math.round(width * 22 / 304));
+  rect1.setAttributeNS("null","y",Math.round(height * 68 / 464));
+  rect1.setAttributeNS("null","width",Math.round(width * 260 / 304));
+  rect1.setAttributeNS("null","height",Math.round(height * 57 / 464));
+  rect1.setAttributeNS("null","rx",13.11);
+  rect1.setAttributeNS("null","ry",13.11);
   rect1.setAttributeNS("null","fill","#cccccc");
 
   //<rect x="22" y="128" width="260" height="57" rx="13.11" ry="13.11" fill="#cccccc" stroke="none" pointer-events="none"/>
   var rect2 = document.createElementNS("http://www.w3.org/2000/svg","rect");
-  rect2.setAttributeNS("null","x",22*w);
-  rect2.setAttributeNS("null","y",128*h);
-  rect2.setAttributeNS("null","width",260*w);
-  rect2.setAttributeNS("null","height",57*h);
-  rect2.setAttributeNS("null","rx",13.11*w);
-  rect2.setAttributeNS("null","ry",13.11*h);
+  rect2.setAttributeNS("null","x",width * 22 / 304);
+  rect2.setAttributeNS("null","y",height * 128 / 464);
+  rect2.setAttributeNS("null","width",width * 260 / 304);
+  rect2.setAttributeNS("null","height",height * 57 / 464);
+  rect2.setAttributeNS("null","rx",13.11);
+  rect2.setAttributeNS("null","ry",13.11);
   rect2.setAttributeNS("null","fill","#cccccc");
 
   //<rect x="42" y="18" width="80" height="60" rx="9" ry="9" fill="#bb0b0b" stroke="#f45a2f" stroke-width="4" pointer-events="none"/>
   var rect3 = document.createElementNS("http://www.w3.org/2000/svg","rect");
-  rect3.setAttributeNS("null","x",48*w);
-  rect3.setAttributeNS("null","y",18*h);
-  rect3.setAttributeNS("null","width",80*w);
-  rect3.setAttributeNS("null","height",60*h);
-  rect3.setAttributeNS("null","rx",9*w);
-  rect3.setAttributeNS("null","ry",9*h);
+  rect3.setAttributeNS("null","x",width * 48 / 304);
+  rect3.setAttributeNS("null","y",width * 18 / 464);
+  rect3.setAttributeNS("null","width",width *80 / w);
+  rect3.setAttributeNS("null","height",height * 60 / h);
+  rect3.setAttributeNS("null","rx",9);
+  rect3.setAttributeNS("null","ry",9);
   rect3.setAttributeNS("null","fill","#bb0b0b");
   rect3.setAttributeNS("null","stroke","#f45a2f");
-  rect3.setAttributeNS("null","stroke-width","4");
+  rect3.setAttributeNS("null","stroke-width",4);
 
   //<rect x="182" y="18" width="80" height="60" rx="9" ry="9" fill="#bb0b0b" stroke="#f45a2f" stroke-width="4" pointer-events="none"/>
   var rect4 = document.createElementNS("http://www.w3.org/2000/svg","rect");
-  rect4.setAttributeNS("null","x",182*w);
-  rect4.setAttributeNS("null","y",18*h);
-  rect4.setAttributeNS("null","width",80*w);
-  rect4.setAttributeNS("null","height",60*h);
-  rect4.setAttributeNS("null","rx",9*w);
-  rect4.setAttributeNS("null","ry",9*h);
+  rect4.setAttributeNS("null","x",width * 182 / 304);
+  rect4.setAttributeNS("null","y",height * 18 / 464);
+  rect4.setAttributeNS("null","width",width*80 / 304);
+  rect4.setAttributeNS("null","height",height*60 / 464);
+  rect4.setAttributeNS("null","rx",9);
+  rect4.setAttributeNS("null","ry",9);
   rect4.setAttributeNS("null","fill","#bb0b0b");
   rect4.setAttributeNS("null","stroke","#f45a2f");
-  rect4.setAttributeNS("null","stroke-width","4");
+  rect4.setAttributeNS("null","stroke-width",4);
 
   //<rect x="2" y="48" width="300" height="410" rx="27" ry="27" fill="#bb0b0b" stroke="#f45a2f" stroke-width="4" pointer-events="none"/>
   var rect5 = document.createElementNS("http://www.w3.org/2000/svg","rect");
-  rect5.setAttributeNS("null","x",2*w);
-  rect5.setAttributeNS("null","y",48*h);
-  rect5.setAttributeNS("null","width",300*w);
-  rect5.setAttributeNS("null","height",410*h);
-  rect5.setAttributeNS("null","rx",27*w);
-  rect5.setAttributeNS("null","ry",27*h);
+  rect5.setAttributeNS("null","x",width * 2 / w);
+  rect5.setAttributeNS("null","y",height * 48 / h);
+  rect5.setAttributeNS("null","width",width * 300 / w);
+  rect5.setAttributeNS("null","height",height * 410 / h);
+  rect5.setAttributeNS("null","rx",27);
+  rect5.setAttributeNS("null","ry",27);
   rect5.setAttributeNS("null","fill","#bb0b0b");
   rect5.setAttributeNS("null","stroke","#f45a2f");
-  rect5.setAttributeNS("null","stroke-width","4");
+  rect5.setAttributeNS("null","stroke-width",4);
 
   //<rect x="44" y="44" width="76" height="21" fill="#bb0b0b" stroke="none" pointer-events="none"/>
   var rect6 = document.createElementNS("http://www.w3.org/2000/svg","rect");
-  rect6.setAttributeNS("null","x",44*w);
-  rect6.setAttributeNS("null","y",44*h);
-  rect6.setAttributeNS("null","width",76*w);
-  rect6.setAttributeNS("null","height",21*h);
+  rect6.setAttributeNS("null","x",width * 44/w);
+  rect6.setAttributeNS("null","y",height*44/h);
+  rect6.setAttributeNS("null","width",width*76/w);
+  rect6.setAttributeNS("null","height",height*21/h);
   rect6.setAttributeNS("null","fill","#bb0b0b");
 
   //<rect x="184" y="40" width="76" height="21" fill="#bb0b0b" stroke="none" pointer-events="none"/>
   var rect7 = document.createElementNS("http://www.w3.org/2000/svg","rect");
-  rect7.setAttributeNS("null","x",184*w);
-  rect7.setAttributeNS("null","y",40*h);
-  rect7.setAttributeNS("null","width",76*w);
-  rect7.setAttributeNS("null","height",21*h);
+  rect7.setAttributeNS("null","x",width * 184 /w);
+  rect7.setAttributeNS("null","y",height * 40 /h);
+  rect7.setAttributeNS("null","width",width * 76/w);
+  rect7.setAttributeNS("null","height",height *21/h);
   rect7.setAttributeNS("null","fill","#bb0b0b");
-
   g.append(rect1);
   g.append(rect2);
   g.append(rect3);
@@ -82,7 +97,6 @@ createBackground = function(width, height) {
   g.append(rect5);
   g.append(rect6);
   g.append(rect7);
-
   return g;
 }
 
@@ -90,11 +104,11 @@ createLevel = function (width,height, soc, soh) {
   var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
   var empty = document.createElementNS("http://www.w3.org/2000/svg", "rect");
 
-  var y = d3.scale.linear().range([0,height]);
+  var y = d3.scaleLinear().range([0,height]);
   y.domain([0,100]);
 
-  empty.setAttributeNS("null","rx",width * 20);
-  empty.setAttributeNS("null","ry",width * 20);
+  empty.setAttributeNS("null","rx",20);
+  empty.setAttributeNS("null","ry",20);
   empty.setAttributeNS("null","fill","darkgray");
   empty.setAttributeNS("null","x",0);
   empty.setAttributeNS("null","y",y(0));
@@ -129,26 +143,6 @@ createView = function(width,height) {
   svg.setAttributeNS(null, "style","background-color: rgb(255, 255, 255);");
   var g = createBackground(width, height);
   svg.append(g);
-  g.append(createLevel(width, height,0,0));
-  document.getElementById("BateryStateView").append(svg);
+//   g.append(createLevel(width, height,0,0));
+  document.getElementById("BatteryStateView").append(svg);
 }
-
-self.onInit = function () {
-    createView(self.ctx.width, self.ctx.height);
-};
-
-self.onDataUpdated = function () {
-
-};
-
-self.onResize = function () {
-    var myNode = document.getElementById("BateryStateView");
-    while (myNode.firstChild) {
-        myNode.removeChild(myNode.firstChild);
-    }
-    createView(self.ctx.width, self.ctx.height);
-};
-
-self.onDestroy = function () {
-
-};
